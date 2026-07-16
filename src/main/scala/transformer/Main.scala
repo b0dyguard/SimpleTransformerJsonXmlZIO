223789c2c95904060c490ba2c3f0405e6966450a
Aleksandr Tumakov <tumakov@bregis.ru>
2026-07-16 19:56:28 +0400

    version 1.0(stable)

--- src/main/scala/transformer/Main.scala
+++ src/main/scala/transformer/Main.scala
@@ -13,23 +13,30 @@ object Main extends ZIOAppDefault {
   override val bootstrap: ZLayer[ZIOAppArgs, Any, Any] = Runtime.removeDefaultLoggers >>> SLF4J.slf4j
 
   override def run: ZIO[Any with ZIOAppArgs with Scope, Any, Any] = {
-    val appLogic = for {
-      _ <- ZIO.logInfo("Initializing Database..")
-      db <- ZIO.service[DatabaseService]
-      _ <- db.initDb
+    val program = for {
+      _ <- ZIO.logInfo("Initializing application components..")
+
+      dbService <- ZIO.service[DatabaseService]
+      _ <- dbService.initDb
       _ <- ZIO.logInfo("Database successfully initialized.")
 
-      exporter <- ZIO.service[CsvExportService]
-      _ <- exporter.exporting
+      csvService <- ZIO.service[CsvExportService]
+      _ <- csvService.exporting
+      _ <- ZIO.logInfo("CSV Export service successfully started.")
 
       configService <- ZIO.service[ConfigService]
       port = configService.config.port
 
-      httpService <- ZIO.service[HttpService]
-      routes = httpService.routes
+      httpService  <- ZIO.service[HttpService]
+      _            <- ZIO.logInfo(s"Starting HTTP Server on port $port..")
+      serverConfig = Server.Config.default.port(port)
+      _            <- Server.serve(httpService.routes).provideSome[Scope](
+        Server.live,
+        ZLayer.succeed(serverConfig)
+      )
     } yield ()
 
-    appLogic.provide(
+    program.provideSome[Scope](
       ConfigService.live,
       DatabaseService.live,
       XmlMapperService.live,
