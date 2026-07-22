b89582ccab5d453067f1af8d19d6e6acb8654ba2
Aleksandr Tumakov <tumakov@bregis.ru>
2026-07-22 16:43:06 +0400

    version 1.3.1(stable)

---
+++ src/main/scala/transformer/config/AppConfig.scala
@@ -0,0 +1,10 @@
+package transformer.config
+
+import zio.json._
+
+case class AppConfig(port: Int, intervalMinutes: Int, targetDirectory: String)
+
+
+object AppConfig {
+  implicit val encoder: JsonEncoder[AppConfig] = DeriveJsonEncoder.gen[AppConfig]
+}
