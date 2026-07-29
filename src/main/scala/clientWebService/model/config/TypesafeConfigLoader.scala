aa87e8ebc8dc9b8713f36bd127009073907642a4
Aleksandr Tumakov <tumakov@bregis.ru>
2026-07-29 19:29:21 +0400

    config params reading on ZIO Fibers

--- src/main/scala/clientWebService/model/config/TypesafeConfigLoader.scala
+++ src/main/scala/clientWebService/model/config/TypesafeConfigLoader.scala
@@ -11,12 +11,12 @@ import scala.util.Try
 object TypesafeConfigLoader {
   val live: ZLayer[Any, Throwable, AppConfig] = ZLayer {
     for {
-      _ <- ZIO.logInfo("Loading configuration properties..")
+      _      <- ZIO.logInfo("Loading configuration properties..")
       tsConf <- ZIO.attempt(ConfigFactory.load())
 
-      port = Try(tsConf.getInt("port")).getOrElse(9000)
-      interval = Try(tsConf.getInt("intervalMinutes")).getOrElse(1)
-      dir = Try(tsConf.getString("targetDirectory")).map(_.trim).filter(_.nonEmpty).getOrElse("C:/TEMP/Export")
+      port      <- ZIO.attempt(Try(tsConf.getInt("port")).getOrElse(9000))
+      interval  <- ZIO.attempt(Try(tsConf.getInt("intervalMinutes")).getOrElse(1))
+      dir       <- ZIO.attempt(Try(tsConf.getString("targetDirectory")).map(_.trim).filter(_.nonEmpty).getOrElse("C:/TEMP/Export"))
 
       appConfig = AppConfig(port, interval, dir)
       _ <- ZIO.logInfo(s"Configuration loaded successfully: ${appConfig.toJson}")
